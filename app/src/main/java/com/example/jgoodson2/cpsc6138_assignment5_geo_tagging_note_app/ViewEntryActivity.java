d85699295a6fb0644d1423ed0b96be5b276d2bdc
jgoodson2 <jonathongoodson@clayton.edu>
2018-07-25 22:21:40 -0400

    Incremental

--- app/src/main/java/com/example/jgoodson2/cpsc6138_assignment5_geo_tagging_note_app/ViewEntryActivity.java
+++ app/src/main/java/com/example/jgoodson2/cpsc6138_assignment5_geo_tagging_note_app/ViewEntryActivity.java
@@ -2,6 +2,7 @@ package com.example.jgoodson2.cpsc6138_assignment5_geo_tagging_note_app;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +10,25 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ViewEntryActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ViewEntryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
     private static final String TAG = "ViewEntryActivity";
     private TextView lbl_viewEntry_subject, lbl_viewEntry_content, lbl_viewEntry_info;
     private DAO dao;
     private int selectedID;
+    private long latitude, longitude;
     Cursor recordData;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +56,35 @@ public class ViewEntryActivity extends AppCompatActivity {
                         "\nLatitude: " + recordData.getString(4) +
                         "\nLongitude: " + recordData.getString(5)
                 );
+                latitude = recordData.getLong(4);
+                longitude = recordData.getLong(5);
                 lbl_viewEntry_content.setText(recordData.getString(3));
             }
         }
+
+        MapFragment mapFragment = MapFragment.newInstance().newInstance();
+        android.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.map_space, mapFragment);
+        fragmentTransaction.commit();
+
+        mapFragment.getMapAsync(this);
+
     }
 
     public void goBackToViewAllEntries(View view) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady");
+
+        mMap = googleMap;
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 }
